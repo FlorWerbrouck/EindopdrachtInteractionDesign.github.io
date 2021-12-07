03aa37b67bf44befc253b3ef339e6baa56b95470
FlorWerbrouck <flor.werbrouck@student.howest.be>
2021-12-07 23:22:35 +0100

    my first commit

---
+++ script/app.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+let getAPI = async () => {
+	// Eerst bouwen we onze url op
+	const ENDPOINT = `https://api.spacexdata.com/v4/launches/next`;
+
+	// Met de fetch API proberen we de data op te halen.
+	const request = await fetch(`${ENDPOINT}`);
+	const data = await request.json();
+	console.log(data);
+
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+	
+	getAPI();
+});
