8ad87ac5188b62daf9518bcec3aad51d5303e4e6
FlorWerbrouck <72013803+FlorWerbrouck@users.noreply.github.com>
2021-12-21 19:49:31 +0100

    launchpad + structure get api

--- script/app.js
+++ script/app.js
@@ -2,10 +2,14 @@ var htmlFlight;
 var htmlTime;
 var htmlDate;
 var htmlRocket;
+var htmlLaunchpad;
+var htmlRegion;
 
 var nextLaunch;
 var rocket;
 var flightnr;
+var launchpad;
+var region;
 
 function getTimeRemaining(endtime){
 	const total = Date.parse(endtime) - Date.parse(new Date());
@@ -31,6 +35,8 @@ const showResult = () => {
 	htmlTime.innerHTML = t.days + ' days, ' + t.hours + ' hours, ' + t.minutes + ' minutes and ' + t.seconds + ' seconds';
 	htmlDate.innerHTML = nextLaunch;
 	htmlRocket.innerHTML = rocket;
+	htmlLaunchpad.innerHTML = launchpad;
+	htmlRegion.innerHTML = region;
 
 	var intervalId = window.setInterval(function(){
 		replaceResult()
@@ -44,25 +50,46 @@ function replaceResult(){
 	htmlTime.innerHTML = t.days + ' days, ' + t.hours + ' hours, ' + t.minutes + ' minutes and ' + t.seconds + ' seconds';
 }
 
-let getAPI = async () => {
-	// Eerst bouwen we onze url op
+let getNextLaunch = async () => {
 	const ENDPOINT = `https://api.spacexdata.com/v4/launches/next`;
-	const ENDPOINT2 = `https://api.spacexdata.com/v4/rockets/`;
-
-	// Met de fetch API proberen we de data op te halen.
+	
 	const request = await fetch(`${ENDPOINT}`);
 	const dataLaunch = await request.json();
+	
 	var utcDate = dataLaunch.date_utc;
 	var localDate = new Date(utcDate);
 	
 	nextLaunch = localDate;
-	flightnr = dataLaunch.flight_number;
-	console.log(flightnr);
-	
-	const request2 = await fetch(`${ENDPOINT2 + dataLaunch.rocket}`);
-	const dataRocket = await request2.json();
-	rocket = dataRocket.name;
 
+	flightnr = dataLaunch.flight_number;
+
+	var rocketID = dataLaunch.rocket;
+	await getRocket(rocketID);
+
+	var launchpadID = dataLaunch.launchpad
+	await getLaunchpad(launchpadID)
+}
+
+let getRocket = async (rocketID) => {
+	const ENDPOINT = `https://api.spacexdata.com/v4/rockets/` + rocketID;
+
+	const request = await fetch(`${ENDPOINT}`);
+	const dataRocket = await request.json();
+	rocket = dataRocket.name;
+}
+
+let getLaunchpad = async (launchpadID) => {
+	const ENDPOINT = `https://api.spacexdata.com/v4/launchpads/` + launchpadID;
+	const request = await fetch(`${ENDPOINT}`);
+	const dataLaunchpad = await request.json();
+	
+	launchpad = dataLaunchpad.full_name;
+	region = dataLaunchpad.region;
+}
+
+let getAPI = async () => {
+
+	await getNextLaunch();
 	showResult();
 
 };
@@ -72,5 +99,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	htmlTime = document.querySelector('.js-time');
 	htmlDate = document.querySelector('.js-date');
 	htmlRocket = document.querySelector('.js-rocket');
+	htmlLaunchpad = document.querySelector('.js-launchpad');
+	htmlRegion = document.querySelector('.js-region');
 	getAPI();
 });
